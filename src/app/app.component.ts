19728e7d6907ccbdb440bce07f25bba5e6e8d848
alejandro7536 <alejandroalas7536@gmail.com>
2022-01-09 17:44:28 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stepper';
+
+    userForm!: FormGroup;
+    doctorForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.initForms();
+  }
+
+
+
+  initForms() {
+    this.userForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      firstName: ['', [Validators.required, Validators.minLength(5)]],
+      lastName: ['', [Validators.required, Validators.minLength(5)]]
+    });
+
+    this.doctorForm = this.fb.group({
+      especialidad: ['', [Validators.required, Validators.minLength(5)]],
+      licenciaProfesional: ['', [Validators.required, Validators.minLength(5)]],
+      universidad: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  get invalidFirstname() {
+    return this.userForm?.get('firstName')?.invalid && this.userForm?.get('firstName')?.touched;
+  }
+
+  get invalidLastname() {
+    return this.userForm.get('lastName')?.invalid && this.userForm.get('lastName')?.touched;
+  }
+
+  get invalidEmail() {
+    return this.userForm.get('email')?.invalid && this.userForm.get('email')?.touched;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CdkStepperComponent } from './components/cdk-stepper/cdk-stepper.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkStepperModule} from '@angular/cdk/stepper';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CdkStepperComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    CdkStepperModule,
+    ReactiveFormsModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
